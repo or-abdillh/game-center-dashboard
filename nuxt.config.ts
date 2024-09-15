dcfd658d38f73e354f04510f1bd7ecd88f4c9c2e
or-abdillh <hans.abdillh05@gmail.com>
2024-09-15 09:06:56 +0800

    feat: add tailwindcss, google-fonts, and nuxt-mdi dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+    compatibilityDate: '2024-04-03',
+    devtools: { enabled: true },
+    modules: ['@nuxtjs/tailwindcss', '@nuxtjs/google-fonts', 'nuxt-mdi'],
+    googleFonts: {
+        families: {
+            Poppins: {
+                wght: [400, 500, 600, 700],
+                italic: [400, 500, 600, 700],
+            }
+        },
+        display: 'swap',
+        preconnect: true,
+        preload: true,
+        stylePath: "css",
+        outputDir: "assets",
+        fontsDir: "fonts",
+    }
 })
